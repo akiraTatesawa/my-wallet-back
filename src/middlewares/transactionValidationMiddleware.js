65a04e2d9c21a42f7aa43d2c05ce0700bf5fbe61
Arthur Akira <arthurak10.at@gmail.com>
2022-07-02 18:55:18 -0300

    fix: invalidate transactions values that contain a dot

--- src/middlewares/transactionValidationMiddleware.js
+++ src/middlewares/transactionValidationMiddleware.js
@@ -5,17 +5,24 @@ import { transactionSchema } from "../schemas/transactionsSchema.js";
 export function validateTransaction(req, res, next) {
   const { error } = transactionSchema.validate(req.body);
 
+  const { value } = req.body;
+
   if (
     error ||
-    parseFloat(req.body.value.replace(",", "")) === 0 ||
+    value.includes(".") ||
+    parseFloat(value.replace(",", "")) === 0 ||
     stripHtml(req.body.description).result.trim().length === 0
   ) {
     return res.sendStatus(422);
   }
 
+  const formatedValue = parseFloat(req.body.value.replace(",", "."))
+    .toFixed(2)
+    .replace(".", ",");
+
   const transaction = {
     ...req.body,
-    value: req.body.value.replace(".", "").replace(/,0$/, ",00"),
+    value: formatedValue,
     description: stripHtml(req.body.description).result.trim(),
   };
 
